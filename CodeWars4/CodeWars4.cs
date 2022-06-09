dbf2ee5c41ee79a308a454ef7d812dd1c5b82f2a
Daniel Laptop <daniel.barczynski@microsoft.wsei.edu.pl>
2022-06-10 01:44:14 +0200

    update

---
+++ CodeWars4/CodeWars4.cs
@@ -0,0 +1,27 @@
+﻿// Welcome to the city
+
+Console.WriteLine(Kata.SayHello(new string[] {"John", "Smith"}, "Phoenix", "Arizona"));
+Console.WriteLine(Kata.SayHello(new string[] {"John", "Bruce", "Smith"}, "Phoenix", "Arizona"));
+
+Console.WriteLine(Kata.SayHello2(new string[] {"John", "Smith"}, "Phoenix", "Arizona"));
+Console.WriteLine(Kata.SayHello2(new string[] {"John", "Bruce", "Smith"}, "Phoenix", "Arizona"));
+
+public class Kata
+{
+    public static string SayHello(string[] name, string city, string state)
+    {
+        string fullName = "";
+        for (int i = 0; i < name.Length; i++)
+        {
+            fullName += name[i];
+            if(i != name.Length - 1)
+            {
+                fullName += " ";
+            }
+        }
+
+        return $"Hello, {fullName}! Welcome to {city}, {state}!";
+    }
+    public static string SayHello2(string[] name, string city, string state) =>
+    $"Hello, {string.Join(" ", name)}! Welcome to {city}, {state}!";
+}
