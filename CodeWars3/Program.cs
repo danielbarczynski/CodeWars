06a2fdcddccb055a230d905d661e760baf54a71e
Daniel PC <daniel.barczynski@onet.pl>
2022-06-09 09:37:25 +0200

    Add project files.

---
+++ CodeWars3/Program.cs
@@ -0,0 +1,36 @@
+﻿// Descending order
+
+int num = 254342;
+Console.WriteLine(Kata.DescendingOrder(num));
+Console.WriteLine(Kata.DescendingOrder2(num));
+
+public static class Kata
+{
+    public static int DescendingOrder(int num)
+    {
+        string text = num.ToString();
+        var arr = text.ToArray();
+        List<char> list = new List<char>();
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            list.Add(arr[i]);           
+        }
+
+        var q1 = list.OrderByDescending(x => x);
+        q1.ToList();
+        string empty = " ";
+
+        foreach (var x in q1)
+        {
+            empty += x;
+        }
+
+        return Convert.ToInt32(empty);
+    }
+
+    public static int DescendingOrder2(int num)
+    {
+        return int.Parse(string.Concat(num.ToString().OrderByDescending(x => x)));
+    }
+}
