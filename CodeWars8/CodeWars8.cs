3b7cf6eb1f91340804a6d12bd4cbce893e5f049a
Daniel Laptop <daniel.barczynski@microsoft.wsei.edu.pl>
2022-06-13 18:56:06 +0200

    update

--- CodeWars8/CodeWars8.cs
+++ CodeWars8/CodeWars8.cs
@@ -1,16 +1,32 @@
 ﻿using System;
 using System.Linq;
 string words = "is2 Thi1s T4est 3a";
-System.Console.WriteLine(Kata.Order(words));
+Console.WriteLine(Kata.Order(words));
 
 public static class Kata
 {
   public static string Order(string words)
   {     
     string result = "";
-    var splitted = words.Split(' ');
-    var list = splitted.OrderByDescending(x=>Convert.ToInt32(x) > 0);
-    list.ToList().ForEach(x=> result += x.ToString());
-    return result;
+        var splitted = words.Split(' ').ToList();
+
+        for (int i = 0; i < 10; i++)
+        {
+            //if (splitted.Contains(i.ToString()))
+            //{
+
+            //}
+            splitted.OrderBy(x => x.Contains(i.ToString()));
+            
+        }
+        foreach (var item in splitted)
+        {
+            Console.WriteLine(item);
+        }
+       // list.ToList().ForEach(x => result += x);
+        // var list = splitted.OrderBy(x=> x);
+        //list.ToList().ForEach(x=> result += x);
+
+        return result;
   }
 }
