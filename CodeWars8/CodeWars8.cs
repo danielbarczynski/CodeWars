920ffe779438790cd87d00b7dbda62996bba5561
Daniel Laptop <daniel.barczynski@microsoft.wsei.edu.pl>
2022-06-13 18:12:44 +0200

    update

---
+++ CodeWars8/CodeWars8.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+string words = "is2 Thi1s T4est 3a";
+System.Console.WriteLine(Kata.Order(words));
+
+public static class Kata
+{
+  public static string Order(string words)
+  {     
+    string result = "";
+    var splitted = words.Split(' ');
+    var list = splitted.OrderByDescending(x=>Convert.ToInt32(x) > 0);
+    list.ToList().ForEach(x=> result += x.ToString());
+    return result;
+  }
+}
