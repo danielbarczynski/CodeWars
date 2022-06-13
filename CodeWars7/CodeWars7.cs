51aa8ff3be88a831292c60adfa4f61bd5d974a0a
Daniel Laptop <daniel.barczynski@microsoft.wsei.edu.pl>
2022-06-13 10:40:54 +0200

    update

--- CodeWars7/CodeWars7.cs
+++ CodeWars7/CodeWars7.cs
@@ -1,15 +1,32 @@
-﻿System.Console.WriteLine(new Kata().Min(new []{-52, 56, 30, 29, -54, 0, -110}));
-System.Console.WriteLine(new Kata().Max(new []{-52, 56, 30, 29, -54, 0, -110}));
+﻿using System.Linq;
+using System;
 
-public class Kata
+System.Console.WriteLine(Kata.Solution(20));
+System.Console.WriteLine(Kata.Solution2(20));
+public static class Kata
 {
-  public int Min(int[] list)
+  public static int Solution(int value)
   {
-    return list.Min();
+    int sum = 0;
+     if ( value < 0)
+     {
+    return 0;
+    }
+    
+    for (int i = 0; i < value; i++)
+    {
+      if (i % 5 == 0 || i % 3 == 0){
+        sum += i;
+      }
+    }
+    
+    return sum; 
   }
-  
-  public int Max(int[] list)
+  // Slower and doesn't recognize negative numbers
+  public static int Solution2(int n)
   {
-    return list.Max();
-  }
+   return Enumerable.Range(0, n).Where(e => e % 3 == 0 || e % 5 == 0).Sum();
+   }
+   
 }
+// 3,5,6,9,10,12,15,18 = 78
