1bb4e49faacd7da80b01b973b2fde7fa2c9260dd
Daniel PC <daniel.barczynski@onet.pl>
2022-06-16 15:29:35 +0200

    update

--- RentalsUnited/Program.cs
+++ RentalsUnited/Program.cs
@@ -3,26 +3,30 @@ sentence, if has a value, has words separated by a space
 the dot can be at the end of the sentence
 After reversing the sentence, the dot always have to be at the end of the sentence
 
-Input: "Ala ma kota.", "Sentence with a-dash"
+Input: "Ala ma kota.", "Sentence with a-dash."
 Output: "kota ma Ala.", "a-dash with Sentence."
 */
+
 string ex1 = "Ala ma kota.";
+string ex2 = "Sentence with a-dash.";
 System.Console.WriteLine(RentalsUnited.ReverseWordsInSentence(ex1));
+System.Console.WriteLine(RentalsUnited.ReverseWordsInSentence(ex2));
 
  class RentalsUnited
 {
     public static string ReverseWordsInSentence(string s)
     {
-        var splitted = s.Split(' ');
+        var trimmed = s.TrimEnd('.');
+        var splitted = trimmed.Split();
         var splitted2 = splitted.Reverse();
-        var result = Convert.ToString(splitted2);
-     
-        return result;
-        // string result = "";
-        // for (int i = s.Length - 1; i >= 0; i--)
-        // {
-        //     result += s[i];
-        // }
-        // return result;
+        string result = "";
+
+        foreach (var item in splitted2)
+        {
+            result += item + " ";
+        }
+
+        string result2 = result.TrimEnd() + ".";
+        return result2;
     }
 }
